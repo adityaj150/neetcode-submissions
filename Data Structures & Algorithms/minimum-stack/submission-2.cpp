6bdbdf70074243e000675fa2d98490f649ca7d3d
Aditya Jadhav <162099782+adityaj150@users.noreply.github.com>
2026-08-26 00:15:15 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.cpp
@@ -0,0 +1,27 @@
+class MinStack {
+private:
+    stack<int>s;
+    stack<int>minstack;
+
+public:
+    MinStack() {}
+    
+    void push(int val) {
+        s.push(val);
+        val = min(val, minstack.empty() ? val : minstack.top());
+        minstack.push(val);
+    }
+    
+    void pop() {
+        s.pop();
+        minstack.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return minstack.top();
+    }
+};
